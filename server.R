0dab3748b62c87fb4c81663a5b7853906ddf03d8
Ocidental <35012480+Ocidental@users.noreply.github.com>
2018-01-01 23:11:04 -0200

    Update server.R

--- server.R
+++ server.R
@@ -3,7 +3,8 @@ dados=read.csv2("energia.csv",header=T)
 saida=dados[,2:6]
 rownames(saida)=dados[,1]
 shinyServer(function(input,output){
+  if (input$classe=="comercial") coluna=1
   output$table<-renderTable(
-    summary(saida[,input$classe])
+    summary(saida[,coluna])
   )
 })
